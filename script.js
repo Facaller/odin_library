29346d339468c709403b5c4bacbd86e6720eb9b8
Facaller <ashwin.metal13@gmail.com>
2024-08-09 15:37:59 +0200

    Create form and divs, Create links between elements and script in JS

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const myLibrary = [];
+const author = document.getElementById('author').value;
+const title = document.getElementById('title').value;
+const year = document.getElementById('year').value;
+const pages = document.getElementById('pages').value;
+const read = document.getElementById('read').value;
 
-let mainContent = document.querySelector('.main-content');
+document.getElementById('cardAuthor').textContent = `Author: ${author}`
+document.getElementById('cardAuthor').textContent = `Title: ${title}`
+document.getElementById('cardAuthor').textContent = `Year: ${year}`
+document.getElementById('cardAuthor').textContent = `Pages: ${pages}`
+document.getElementById('cardAuthor').textContent = `Read: ${read}`
 
-function Book (author, title, year, read) {
+document.getElementsByClassName("card-form").addEventListener('submit', function (event) {
+    event.preventDefault()
+})
+
+function Book (author, title, year, pages, read) {
     this.author = author;
-    this.title = title;
-    this.year = year;
-    this.read = read;
+    this.title  = title;
+    this.year   = year;
+    this.pages  = pages;
+    this.read   = read;
 };
 
 function addBookToLibrary (author, title, year, read) {
@@ -14,16 +28,16 @@ function addBookToLibrary (author, title, year, read) {
     myLibrary.push(newBook);
 };
 
-addBookToLibrary ("LOTR", "Tolkein", 1950, "No")
-addBookToLibrary ("Thus Spoke Zarathustra", "Nietzsche", 1940, "Yes");
-addBookToLibrary ("Stranger", "Camus", 1939, "Yes");
+addBookToLibrary ("LOTR", "Tolkein", 1950, 342, "No")
+addBookToLibrary ("Thus Spoke Zarathustra", "Nietzsche", 1940, 213, "Yes");
+addBookToLibrary ("Stranger", "Camus", 1939, 125, "Yes");
 
 function displayBook () {
     let displayText = '';
     for (const obj of myLibrary) {
         displayText += `Author: ${obj.author}, Title: ${obj.title}, Year: ${obj.year}, Read: ${obj.read}\n`;
     }
-    mainContent.textContent = displayText;
+    
 };
 
 displayBook();
